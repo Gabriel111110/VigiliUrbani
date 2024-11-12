2ef73b31fbd7198c916a7f3c2330abf33ab59380
Emanuele Meregalli <c.meregalliemanuele@itis-molinari.eu>
2024-11-12 14:15:16 +0100

    inzio progetto

---
+++ index.js
@@ -0,0 +1,44 @@
+import { generateMap } from "./map.js";
+import { fetchComponent } from "./fetch.js";
+let aggiungiIncidente = document.querySelector("#aggiungiIncidente");
+const apiTokenLocation = "pk.215a9e370a8ded2ada287a67d2b90aaf";
+const btnInvia=document.querySelector("#prenotaButton")//bottone per inviare l'indirizzo
+const indirizzo=document.querySelector("#idIndirizzo");//campo i testo dove inserire l'indirizzo
+const popup=document.getElementById("popup");//campo i testo dove inserire l'indirizzo
+const data=document.getElementById("idData");//campo i testo dove inserire l'indirizzo
+const ora=document.getElementById("idOra");
+const morti=document.getElementById("idMorti");//campo i testo dove inserire l'indirizzo
+const feriti=document.getElementById("idFeriti");//campo i testo dove inserire l'indirizzo
+
+let map = generateMap();
+let fetchC = fetchComponent();
+fetchC.build("cb6e2971-c0e8-4b36-99a3-4792429bab2f");
+map.build();
+fetchC.getData("places").then(res =>{
+    map.addAllPlaces(res)
+    map.render();
+})
+
+btnInvia.onclick=()=>{
+    let url ="https://us1.locationiq.com/v1/search?key="+apiTokenLocation+"&q=%LOCATION, Milano&format=json&";
+    let street = indirizzo.value;
+    url = url.replace("%LOCATION",street)
+    console.log(url);
+    fetch(url)
+    .then(data => data.json())
+    .then(result => {
+        console.log(result);
+        let name = result[0].display_name
+        let lat = result[0].lat
+        let lon = result[0].lon
+        let data = {
+            name : name,
+            coords : [lat,lon]
+        }
+        indirizzo.value="";
+        map.addPlace(data)
+        map.render();
+        fetchC.setData("places",map.getPlaces());
+        
+    });
+}
---
+++ map.js
@@ -0,0 +1,49 @@
+
+
+export const generateMap = () => {
+    let places = [
+       {
+          name: "Piazza del Duomo",
+          coords: [45.4639102, 9.1906426]
+       }
+    ];
+ 
+    
+    const zoom = 12;
+    const maxZoom = 19;
+    let map; 
+ 
+    return {
+     
+       build: () => {
+          map = L.map("map").setView(places[0].coords, zoom);
+       },
+ 
+     
+       render: () => {
+          
+          L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+             maxZoom: maxZoom,
+             attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          }).addTo(map);
+ 
+          places.forEach((place) => {
+             const marker = L.marker(place.coords).addTo(map);
+             marker.bindPopup(`<b>${place.name}</b>`);
+          });
+       },
+ 
+ 
+       addPlace: (place) => {
+          places.push(place);
+          console.log(places)
+       },
+       getPlaces: ()=>{
+          return places;
+       },
+       addAllPlaces: (p)=>{
+          places = p;
+       }
+    };
+ };
+ 
